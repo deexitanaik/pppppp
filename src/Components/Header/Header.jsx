79aa7b86298f7288272c991d37076ed966d7304f
deexitanaik <deexitanaik@gmail.com>
2025-04-22 12:06:04 +0530

    first commi2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header1</div>
+    <div className="container">
+        <div className="cdf">bhbsjhs</div>
+        <div className="bcbnbhfb">
+            
+        </div>
+    </div>
+    
   )
 }
 
-export default Header
+export default Header;
